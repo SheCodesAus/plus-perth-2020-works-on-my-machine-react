cc96ac2d35eb2deaaf162f0cd94a070d7db0cb38
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-24 11:42:06 +0800

    Nav Bar started

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  //template
+  return (
+    <div className="Menu">
+      <Link to="/">HomePage</Link>
+    </div>
+  );
+}
+export default Nav;
