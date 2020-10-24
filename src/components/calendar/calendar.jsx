5810287f126f1d2629b85a5da6eca9e6af9eaaa2
Bachemedei <bronwyn.1992@gmail.com>
2020-10-24 14:50:41 +0800

    calendar working

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,21 +1,56 @@
 import React from "react";
-import BigCalendar from "react-big-calendar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 
-function calendar() {
-  // Set up the localizer for date formatting
-  const localizer = BigCalendar.momentLocalizer(moment);
+const createEvent = ({ start, end }) => {
+  console.log({ start, end });
+};
 
+function MyCalendar() {
+  // Set up the localizer for date formatting
+  const localizer = momentLocalizer(moment);
+
+  // Specify current time
+  const now = new Date();
+
+  // Add dummy event data
+  const events = [
+    {
+      id: 0,
+      title: "All Day Event very long title",
+      allDay: true,
+      start: new Date(2020, 9, 10),
+      end: new Date(2020, 9, 11),
+    },
+    {
+      id: 1,
+      title: "Long Event",
+      start: new Date(2020, 9, 7),
+      end: new Date(2020, 9, 10),
+    },
+    {
+      id: 2,
+      title: "Right now Time Event",
+      start: now,
+      end: now,
+    },
+  ];
+  console.log(events);
   return (
     <div style={{ height: "500pt" }}>
-      <BigCalendar
+      <Calendar
+        events={events}
         startAccessor="start"
         endAccessor="end"
         defaultDate={moment().toDate()}
         localizer={localizer}
+        views={["month", "week", "day"]}
+        selectable={true}
+        onSelectSlot={createEvent}
       />
     </div>
   );
 }
 
-export default calendar;
+export default MyCalendar;
