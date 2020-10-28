5dd04d6ad8cb09933997eef987070225f676c678
Bachemedei <bronwyn.1992@gmail.com>
2020-10-28 20:20:57 +0800

    Adding styling for calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Calendar from "./components/calendar/Calendar";
+import Calendar from "./components/Calendar/Calendar";
 
 function App() {
   return (
--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
+import "./Calendar.css";
 
 const createEvent = ({ start, end }) => {
   console.log({ start, end });
@@ -19,7 +20,7 @@ function MyCalendar() {
     {
       id: 0,
       title: "All Day Event very long title",
-      allDay: true,
+      allDay: false,
       start: new Date(2020, 9, 10),
       end: new Date(2020, 9, 11),
     },
