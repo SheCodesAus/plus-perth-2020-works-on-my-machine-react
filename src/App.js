98892a1d0021b93829f7606e8e6b0df94e812bb1
Bachemedei <bronwyn.1992@gmail.com>
2020-10-29 10:17:03 +0800

    Removed calendar from home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import Calendar from "./components/Calendar/Calendar";
 
 function App() {
-  return (
-    <div className="App">
-      <Calendar />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
