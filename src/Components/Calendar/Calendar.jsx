4b589d136f9de84ff1abd2afa62daf7d431c8241
Bachemedei <bronwyn.1992@gmail.com>
2020-10-29 10:25:28 +0800

    Renaming with correct case

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
-import "./calendar1.css";
+import "./Calendar.css";
 
 const createEvent = ({ start, end }) => {
   console.log({ start, end });
