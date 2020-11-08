4fd41168fa81b2cb5ad687e07f92beaa17d1e3a1
Bachemedei <bronwyn.1992@gmail.com>
2020-11-08 21:54:02 +0800

    still working on event detail styling

--- src/Components/EventDetail/EventDetail.jsx
+++ src/Components/EventDetail/EventDetail.jsx
@@ -6,6 +6,19 @@ function EventDetail(eventDetail) {
   const event = eventDetail.event;
   const start = convertDateTime(event.event_start);
   const end = convertDateTime(event.event_end);
+  const attending = [];
+  const no_response = [];
+  const declined = [];
+
+  event.attendance_set.map((mentor, key) => {
+    if (mentor.status === "accepted") {
+      attending.push(mentor.mentor.mentor_name);
+    } else if (mentor.status === "declined") {
+      declined.push(mentor.mentor.mentor_name);
+    } else {
+      no_response.push(mentor.mentor.mentor_name);
+    }
+  });
 
   const attendingStatus = (status) => {
     if (status === "accepted") {
@@ -28,16 +41,38 @@ function EventDetail(eventDetail) {
         {start} to {end}
       </p>
       <p>{event.event_location}</p>
-      {event.attendance_set.map((mentor, key) => {
-        console.log(mentor.mentor);
-        return (
-          <div>
-            <p className={attendingStatus(mentor.status)}>
-              {mentor.mentor.mentor_name}
-            </p>
+      <div className="status">
+        {attending.length > 0 ? (
+          <div className="attending">
+            <h3>Attending</h3>
+            {attending.map((mentor, key) => (
+              <p>{mentor}</p>
+            ))}
           </div>
-        );
-      })}
+        ) : (
+          <></>
+        )}
+        {no_response.length > 0 ? (
+          <div className="no-response">
+            <h3>Not Responded</h3>
+            {no_response.map((mentor, key) => (
+              <p>{mentor}</p>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+        {declined.length > 0 ? (
+          <div className="declined">
+            <h3>Declined</h3>
+            {declined.map((mentor, key) => (
+              <p>{mentor}</p>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
