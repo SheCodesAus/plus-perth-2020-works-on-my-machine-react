99bd1f4213b08fe76303af9ef5aa13003e445eab
Bronwyn Achemedei <Bronwyn@hatchds-MacBook-Pro.local>
2020-11-14 09:36:31 +0800

    Added confirm modal for delete event. No functionality yet

--- src/Components/DeleteButton/DeleteButton.jsx
+++ src/Components/DeleteButton/DeleteButton.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import "./DeleteButton.css";
 import DeleteIcon from "../Images/delete.svg";
+import ConfirmDelete from "../ConfirmDelete/ConfirmDelete";
 
 function DeleteButton() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  // Set to app root
+  Modal.setAppElement("#root");
+  // Modal methods
+  const openModal = () => {
+    setIsOpen(true);
+    console.log("delete");
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <button className="delete-btn">
-      <img src={DeleteIcon} alt="Delete Icon" />
-    </button>
+    <div>
+      <button className="delete-btn" onClick={openModal}>
+        <img src={DeleteIcon} alt="Delete Icon" />
+      </button>
+      <Modal
+        className="confirm-delete-modal"
+        isOpen={modalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Confirm Delete"
+        shouldFocusAfterRender="true"
+      >
+        <ConfirmDelete />
+      </Modal>
+    </div>
   );
 }
 
