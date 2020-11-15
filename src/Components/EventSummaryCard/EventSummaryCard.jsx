842153e9955b966e9146f320fa73f4fd33943be3
Bronwyn Achemedei <bronwyn.1992@gmail.com>
2020-11-15 15:52:54 +0800

    Made changes to layout of profile page and styling for mentor invites. Fixed date time. Split content up into components

---
+++ src/Components/EventSummaryCard/EventSummaryCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { convertDateTime } from "../../Helpers/ConvertDateTime";
+import EventTypeTag from "../EventTypeTag/EventTypeTag";
+import "./EventSummaryCard.css";
+
+function EventSummaryCard({ event, mentorId }) {
+  const start = convertDateTime(event.event_start);
+  const end = convertDateTime(event.event_end);
+  let mentorStatus = undefined;
+
+  event.attendance_set.map((mentor, key) => {
+    if (mentor.mentor.id.toString() === mentorId.toString()) {
+      if (mentor.status === "accepted") {
+        return (mentorStatus = "Accepted");
+      } else if (mentor.status === "declined") {
+        return (mentorStatus = "Declined");
+      } else {
+        return (mentorStatus = "Not Reponded");
+      }
+    }
+  });
+
+  return (
+    <div className="event-summary-card">
+      <div className="tags">
+        <EventTypeTag eventType={event.event_type} />
+        <p className="city-tag">{event.event_city}</p>
+      </div>
+      <h3 className="card-subheader">{event.event_name}</h3>
+      <div className="card-details">
+        <p>{start}</p>
+        <p>to</p>
+        <p>{end}</p>
+      </div>
+      <p className="status-tag">{mentorStatus}</p>
+    </div>
+  );
+}
+
+export default EventSummaryCard;
---
+++ src/Components/MentorEvents/MentorEvents.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import "./MentorEvents.css";
+import EventSummaryCard from "../EventSummaryCard/EventSummaryCard";
+import LoadingSpinner from "../FullPageLoader/FullPageLoader";
+
+function MentorEvents() {
+  const token = window.localStorage.getItem("token");
+  const { id } = useParams();
+  const [eventsData, setEventsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (token != null) {
+      fetch(`${process.env.REACT_APP_API_URL}invitations/${id}`)
+        .then((results) => {
+          return results.json();
+        })
+        .then((data) => {
+          setEventsData(data);
+          setLoading(false);
+        });
+    }
+  }, [token]);
+
+  if (loading) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <h4 className="top_card"> Event Invitations </h4>
+      <div className="mentor-events">
+        {eventsData.map((event, key) => {
+          return <EventSummaryCard event={event} mentorId={id} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default MentorEvents;
